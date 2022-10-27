b62e9d2d759c97b188815ccb71e592fef0170f75
David Sevikian <david.sevikian.work@gmail.com>
2022-10-27 11:24:33 +0400

    remove div

--- src/pages/counter-page/counter-page.tsx
+++ src/pages/counter-page/counter-page.tsx
@@ -2,9 +2,5 @@ import React from 'react';
 import { ExerciseCounter } from '../../features/exercise-counter';
 
 export const CounterPage = () => {
-  return (
-    <div>
-      <ExerciseCounter />
-    </div>
-  );
+  return <ExerciseCounter />;
 };
