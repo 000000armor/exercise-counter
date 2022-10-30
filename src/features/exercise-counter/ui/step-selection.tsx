4ca3bdbd4ea5b1ea93848333d938aa6bcc4444f7
David Sevikian <david.sevikian.work@gmail.com>
2022-10-30 14:39:18 +0400

    style step-selection

--- src/features/exercise-counter/ui/step-selection.tsx
+++ src/features/exercise-counter/ui/step-selection.tsx
@@ -11,12 +11,12 @@ export const StepSelection = () => {
     stepSelected(event.target.value);
 
   return (
-    <div>
-      <h4>Step</h4>
+    <div className="mb-6">
+      <h4 className="text-xl">Step</h4>
       <select
         value={step}
         onChange={handleStepChange}
-        className="block p-2 mb-6 w-full h-14 text-sm text-white bg-black rounded-lg border border-white focus:ring-white focus:border-white"
+        className="block p-2 w-full h-14 text-sm text-white bg-black rounded-lg border border-white focus:ring-white focus:border-white"
       >
         {options.map((optionValue) => (
           <option key={optionValue}>{optionValue}</option>
