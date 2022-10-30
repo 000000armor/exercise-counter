a5c80016cc54db5caa09d63863ea24e7fe0a7816
David Sevikian <david.sevikian.work@gmail.com>
2022-10-30 14:39:00 +0400

    refactor

--- src/features/exercise-counter/index.tsx
+++ src/features/exercise-counter/index.tsx
@@ -1,11 +1,11 @@
 import { Counter } from './ui/counter';
+import { Goal } from './ui/goal';
 import { StepSelection } from './ui/step-selection';
 
-export const ExerciseCounter = () => {
-  return (
-    <div>
-      <StepSelection />
-      <Counter />
-    </div>
-  );
-};
+export const ExerciseCounter = () => (
+  <>
+    <Counter />
+    <StepSelection />
+    <Goal />
+  </>
+);
